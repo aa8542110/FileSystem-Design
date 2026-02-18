85b7c05ba934c6e65d7bc75716eb3363fcb62350
Shawn <X>
2026-02-18 23:28:45 +0800

    refactor: 調整大量資料效能潛在問題

--- WinbondProj/Services/FileSystemService.cs
+++ WinbondProj/Services/FileSystemService.cs
@@ -203,7 +203,6 @@ public class FileSystemService : IFileSystemService
             Size = item.Size,
             CreatedDate = item.CreatedDate,
             ParentId = item.ParentId,
-            TotalSize = item.GetTotalSize(),
             Extension = item.GetExtension(),
             Tags = item.Tags.Select(t => new TagDto
             {
@@ -218,19 +217,23 @@ public class FileSystemService : IFileSystemService
             case WordFile wordFile:
                 dto.ItemType = "WordFile";
                 dto.Pages = wordFile.Pages;
+                dto.TotalSize = dto.Size;
                 break;
             case ImageFile imageFile:
                 dto.ItemType = "ImageFile";
                 dto.Width = imageFile.Width;
                 dto.Height = imageFile.Height;
+                dto.TotalSize = dto.Size;
                 break;
             case TextFile textFile:
                 dto.ItemType = "TextFile";
                 dto.Encoding = textFile.Encoding;
+                dto.TotalSize = dto.Size;
                 break;
             case Directory directory:
                 dto.ItemType = "Directory";
                 dto.Items = directory.Items.Select(MapToDto).ToList();
+                dto.TotalSize = dto.Size + dto.Items.Sum(child => child.TotalSize);
                 break;
         }
 
